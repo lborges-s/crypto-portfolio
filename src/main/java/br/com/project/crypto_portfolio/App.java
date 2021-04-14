0ff0d83a2a560bdb31b9b121728d2849f80df10d
Lucas Carmelim <lucasdcarmelim@gmail.com>
2021-04-13 22:56:29 -0300

    Criação da nova tela bacana legal

--- src/main/java/br/com/project/crypto_portfolio/App.java
+++ src/main/java/br/com/project/crypto_portfolio/App.java
@@ -25,7 +25,7 @@ public class App extends Application {
 
     @Override
     public void start(Stage stage) throws IOException {
-        scene = new Scene(_loadFXML("primary"), 640, 480);
+        scene = new Scene(_loadFXML("telaInicial"), 640, 480);
         stage.setScene(scene);
         stage.show();
         
