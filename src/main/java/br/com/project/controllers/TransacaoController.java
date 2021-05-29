ca6f821a24208c8e72fa8e4f6c12db4c55f1ebe0
Lucas Borges de Souza <lucasb.souza01@hotmail.com>
2021-05-29 14:58:04 -0300

    Att transacao

--- src/main/java/br/com/project/controllers/TransacaoController.java
+++ src/main/java/br/com/project/controllers/TransacaoController.java
@@ -1,15 +1,29 @@
 package br.com.project.controllers;
 
+import java.net.URI;
 import java.net.URL;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.net.http.HttpResponse.BodyHandlers;
+import java.time.LocalDate;
 import java.util.ResourceBundle;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
+
+import javax.swing.JOptionPane;
 
 import br.com.project.utils.IController;
 import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
+import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.Cursor;
+import javafx.scene.control.Button;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.TextField;
 import javafx.scene.control.Toggle;
 import javafx.scene.control.ToggleButton;
 import javafx.scene.control.ToggleGroup;
@@ -17,6 +31,7 @@ import javafx.stage.Stage;
 import javafx.stage.WindowEvent;
 
 public class TransacaoController implements Initializable, IController {
+	private static String baseUrl = "https://api.binance.com/api/v3/ticker/price";
 
 	Stage stage;
 
@@ -25,18 +40,41 @@ public class TransacaoController implements Initializable, IController {
 
 	@FXML
 	private ToggleButton btnToggleVender;
+
+	@FXML
+	private TextField txtFieldMoeda;
+
+	@FXML
+	private Button btnPesquisaMoeda;
 	
+	@FXML
+	private Button btnFinalizar;
+	
+    @FXML
+    private DatePicker fieldDtCompra;
+
 	private String tpTransacao;
+	
+	@Override
+	public void initialize(URL location, ResourceBundle resources) {
+		LocalDate localDate = LocalDate.now();
+		fieldDtCompra.setValue(localDate);
+	}
 
 	@Override
 	public void setStage(Stage stage) {
 		this.stage = stage;
 		final ToggleGroup group = new ToggleGroup();
-		
+
 		group.selectedToggleProperty().addListener(new ChangeListener<Toggle>() {
 			public void changed(ObservableValue<? extends Toggle> ov, Toggle toggle, Toggle new_toggle) {
-				System.out.println((String) group.getSelectedToggle().getUserData());
 				tpTransacao = (String) group.getSelectedToggle().getUserData();
+				if(tpTransacao == null)return;
+				if(tpTransacao.equals("C")) {
+					btnFinalizar.setText("COMPRAR");
+				}else {
+					btnFinalizar.setText("VENDER");
+				}
 			}
 		});
 
@@ -48,21 +86,54 @@ public class TransacaoController implements Initializable, IController {
 				btnToggleComprar.setUserData("C");
 				btnToggleComprar.setSelected(true);
 				btnToggleComprar.setCursor(Cursor.HAND);
-				
+
 				btnToggleVender.setToggleGroup(group);
 				btnToggleVender.setUserData("V");
 				btnToggleVender.setCursor(Cursor.HAND);
-				
+
 				System.out.println(btnToggleComprar.getToggleGroup());
 			}
 		});
 	}
-	
-	private void _save() {
-		
-	}
 
-	@Override
-	public void initialize(URL location, ResourceBundle resources) {
+	@FXML
+	private void salvarTransacao(ActionEvent event) {
+		System.out.println("salvarTransacao");
+	}
+	
+	@FXML
+	private void pesquisarMoeda(ActionEvent event) {
+		if(txtFieldMoeda.getText().isBlank()) {
+			JOptionPane.showMessageDialog(null, "Informe a moeda para pesquisar", "Campo Obrigatório",
+					JOptionPane.WARNING_MESSAGE);
+			return;
+		}
+		
+		String url = baseUrl + "?symbol=" + txtFieldMoeda.getText().toUpperCase();
+		final HttpClient client = HttpClient.newBuilder().build();
+		URI uri = URI.create(url);
+
+		HttpRequest req = HttpRequest.newBuilder(uri).GET().build();
+
+		CompletableFuture<HttpResponse<String>> response = client.sendAsync(req, BodyHandlers.ofString());
+		response.thenAcceptAsync(res -> System.out.println(res));
+
+		try {
+			if (response.get().statusCode() == 200) {
+				String body = response.get().body();
+//			BinanceModel model = objectMapper.readValue(body, BinanceModel.class);
+				System.out.println("---------- API --------- ");
+				System.out.println("CHAMOU > " + body);
+				System.out.println("------------------------ ");
+			}else {
+				JOptionPane.showMessageDialog(null, "Não foi possível encontrar a moeda, tente novamente", "Erro ao pesquisar",
+						JOptionPane.ERROR_MESSAGE);
+			}
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		} catch (ExecutionException e) {
+			e.printStackTrace();
+		}
+		
 	}
 }
